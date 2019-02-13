62ae6a15089b6712ed31c5742ff0a1fc25ee71b8
Driss OUDMINE <unbreakabledriss@gmail.com>
2019-02-13 18:36:58 +0100

    Testimonials Section is Finished

--- js/app.js
+++ js/app.js
@@ -49,4 +49,14 @@
 		}
 	});
 
+	// Testimonials carousel (uses the Owl Carousel library)
+	$(".testimonials-carousel").owlCarousel({
+		autoplay: true,
+		dots: true,
+		loop: true,
+		items: 1,
+		smartSpeed: 500,
+		autoplayHoverPause: true
+	});
+
 })(jQuery);
